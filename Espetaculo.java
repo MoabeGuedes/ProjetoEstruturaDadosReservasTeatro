c2036ddb40f00412a4e302ac36227d38030df697
Moabe da Silva Guedes Rêgo <163332682+MoabeGuedes@users.noreply.github.com>
2026-09-08 13:32:40 +0000

    construtor e gets e sets Espetaculo

---
+++ Espetaculo.java
@@ -0,0 +1,64 @@
+public class Espetaculo{
+    private int codigo;
+    private String nome;
+    private String data;
+    private String horario;
+    private double preco;
+    private char[][] assentos;
+
+
+    public Espetaculo(int codigo, String nome, String data, String horario, double preco, char[][] assentos){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.data = data;
+        this.horario = horario;
+        this.preco = preco;
+        this.assentos = assentos;
+        
+    }
+
+
+
+    //gets e sets
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getData() {
+        return data;
+    }
+    public String getHorario() {
+        return horario;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public char[][] getAssentos() {
+        return assentos;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco; 
+    }
+    public void setAssentos(char[][] assentos) {
+        this.assentos = assentos;
+    }
+
+
+
+}
