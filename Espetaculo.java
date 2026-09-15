4c272d646500f396400541007a54c9c9fee08b18
Guigspinho <84827957+Guigspinho@users.noreply.github.com>
2026-09-15 12:53:08 +0000

    teste

--- Espetaculo.java
+++ Espetaculo.java
@@ -9,12 +9,13 @@ public class Espetaculo{
     private char[][] assentos;
 
 
-    public Espetaculo(int codigo, String nome, String data, String horario, double preco){
+    public Espetaculo(int codigo, String nome, String data, String horario, double preco, char[][] assentos){
         this.codigo = codigo;
         this.nome = nome;
         this.data = data;
         this.horario = horario;
         this.preco = preco;
+        this.assentos = new char[5][8];
         
     }
 
--- Teatro.java
+++ Teatro.java
@@ -2,6 +2,7 @@
 Moabe Guedes - 10748053 */
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Teatro {
     private Vetor <Espetaculo> espetaculos;
@@ -28,7 +29,70 @@ public class Teatro {
         arquivo.close();
     }
 
+    public void exibirMenu() {
+        int selecaoMenu;
 
+        do {
+            System.out.println("=== Teatro Mack ===");
+            System.out.println("1. Carregar espetáculos");
+            System.out.println("2. Exibir espetáculos");
+            System.out.println("3. Fazer reserva");
+            System.out.println("4. Consultar mapa de assentos");
+            System.out.println("5. Consultar reserva");
+            System.out.println("6. Estatísticas");
+            System.out.println("7. Sair");
+            System.out.println("========================================");
+            System.out.print("Selecione uma opção: ");
+
+            while (entrada.hasNextInt() == false) {
+                System.out.println("Opção inválida. Digite um número de 1 a 3.");
+                entrada.next();
+            }
+            selecaoMenu = entrada.nextInt();
+            entrada.nextLine();
+
+            switch (selecaoMenu) {
+
+                case 1:
+                    carregaTXT();
+                    System.out.println("Espetáculos Carregados");
+                    break;
+
+                case 2:
+                    System.out.println("1 - Passageiro | 2 - Motorista");
+                    while (entrada.hasNextInt() == false) {
+                        System.out.println("Opção inválida. Digite 1 ou 2.");
+                        entrada.next();
+                    }
+                    int tipo = entrada.nextInt();
+                    entrada.nextLine();
+                    if (tipo == 1) {
+                        Passageiro passageiroLogado = gerUsuarios.logarPassageiro(entrada);
+                        if (passageiroLogado != null) {
+                            exibirMenuPassageiro(entrada, passageiroLogado);
+                        }
+                    } else if (tipo == 2) {
+                        Motorista motoristaLogado = gerUsuarios.logarMotorista(entrada);
+                        if (motoristaLogado != null) {
+                            exibirMenuMotorista(entrada, motoristaLogado);
+                        }
+                    } else {
+                        System.out.println(
+                                "Tipo de conta inválido. Tente novamente.\n"
+                        );
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.\n");
+            }
+
+        } while (selecaoMenu != 3);
+    }
 
 
     public Vetor<Espetaculo> getEspetaculos() {
